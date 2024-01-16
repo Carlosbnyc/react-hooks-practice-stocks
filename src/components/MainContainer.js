34c581646dbca2754dc4397d0b9a67d950de23c3
Carlos <bowyercarlos@gmail.com>
2024-01-15 21:13:55 -0500

    finished lab

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
+
 function MainContainer() {
-  return (
+
+  const [stocks, setStocks] = useState([])
+  const [mysStocks, setMyStocks]= useState([])
+  const [sortBy, setSortBy]= useState("")
+  const [filterBy, setFilterBy]=useState("")
+
+  useEffect (()=>{
+    fetch ("http://localhost:3001/stocks")
+    .then(res => res.json())
+    .then(stocks => {setStocks(stocks)})
+    .catch(err => alert(err))
+  },[]);
+
+  const handleAddStock = (stockToAdd) => {
+    setMyStocks(currentMyStocks => [...currentMyStocks,stockToAdd] )
+
+  }
+ 
+  const handleRemoveStock = (stockToRemove) => {
+    setMyStocks(currentMyStocks => currentMyStocks.filter(stock=> stock.id !== stockToRemove.id))
+  }
+
+  const handleToggleSort = (e) => {
+    setSortBy(e.target.value)
+  }
+
+  const handleFilter = (e) => {
+    setFilterBy(e.target.value)
+  }
+  return ( 
     <div>
-      <SearchBar />
+      <SearchBar sortBy={sortBy} handleToggleSort={handleToggleSort} handleFilter={handleFilter}/>
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+        <StockContainer stocks={stocks} handleAddStock={handleAddStock} sortBy={sortBy} filterBy={filterBy}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer myStocks={mysStocks} handleRemoveStock={handleRemoveStock}/>
         </div>
       </div>
     </div>
